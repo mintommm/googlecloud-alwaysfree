e5c9eb97b3e82b3a64aa115e9ae9414c91698ab2
Terry K <1401820+mintommm@users.noreply.github.com>
2026-07-12 20:59:56 +0900

    fix: force recreate firewall resource to purge 0.0.0.0/0 and assign TTY flag for systemd user session

--- infrastructure/firewall.tf
+++ infrastructure/firewall.tf
@@ -27,7 +27,7 @@ resource "google_compute_firewall" "allow_minecraft_rcon_internal" {
 }
 
 resource "google_compute_firewall" "allow_ssh" {
-  name    = "allow-ssh-ingress"
+  name    = "allow-ssh-iap"
   network = "default"
 
   allow {
@@ -35,8 +35,6 @@ resource "google_compute_firewall" "allow_ssh" {
     ports    = ["22"]
   }
 
-  # GCPベストプラクティス：インターネット全域(0.0.0.0/0)からのポート22アクセスを拒否
-  # GCE IAPセキュアプロキシの認証済みIP帯からの通信のみを独占的に許可
   source_ranges = ["35.235.240.0/20"]
   direction     = "INGRESS"
 }
